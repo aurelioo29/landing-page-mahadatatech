eba65f3d38180e5a77206cb0ef756d5c9d28a0bc
aurelioo29 <aureliolucio5@gmail.com>
2026-03-20 08:11:20 +0700

    feat: integrate with google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,27 @@
 import "tailwindcss/tailwind.css";
 import "@/styles/main.css";
+import Script from "next/script";
 
-import { AppProps } from "next/app";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {/* Google Analytics */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-K8VMCQNNSK"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-K8VMCQNNSK');
+        `}
+      </Script>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
